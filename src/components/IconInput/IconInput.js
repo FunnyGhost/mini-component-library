8bede69ec73ae384c2735ea6bb4ad898a3a054a7
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2022-02-28 11:33:46 +0100

    Implemented reusable IconInput component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,74 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const Wrapper = styled.div`
+  width: var(--width);
+  position: relative;
+  border-bottom: var(--border-width) solid ${COLORS.black};
+  padding-bottom: 4px;
+  padding-top: 4px;
+  font-size: var(--font-size);
+  color: ${COLORS.gray700};
+
+  &:focus-within {
+    outline: solid 1px ${COLORS.primary};
+    outline-offset: 1px;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const InputIcon = styled(Icon)`
+  position: absolute;
+  left: 0;
+  top: 4px;
+  font-size: inherit;
+  color: inherit;
+`;
+
+const Input = styled.input`
+  margin-left: var(--icon-spacing);
+  border: none;
+  color: inherit;
+  font-weight: 700;
+
+  ::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 100;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const styles = {
+  large: {
+    '--font-size': '18px',
+    '--border-width': '2px',
+    '--icon-spacing': '24px',
+  },
+  small: {
+    '--font-size': '14px',
+    '--border-width': '1px',
+    '--icon-spacing': '20px',
+  },
+};
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  return (
+    <Wrapper style={{ '--width': width + 'px', ...styles[size] }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <InputIcon
+        id={icon}
+        size={size === 'small' ? 16 : 20}
+        strokeWidth={size === 'small' ? 1 : 2}
+      />
+      <Input type='text' placeholder={placeholder} />
+    </Wrapper>
+  );
 };
 
 export default IconInput;
